cec52262fbaa483d4ee17db460ccad074e11508e
FineSam <samherubel@gmail.com>
2021-12-06 09:35:53 +0900

    Update create_report.py

--- create_report.py
+++ create_report.py
@@ -1,8 +1,9 @@
 from prometheus_pandas import query
 from matplotlib import pyplot as plt
-import sys
-from varname import nameof
 import re
+from datetime import datetime, date, timedelta
+from dateutil.relativedelta import relativedelta
+import pymsteams
 
 p = query.Prometheus('http://<prometheus>:9090')
 
@@ -10,15 +11,31 @@ def rename_column(column_name):
     match = re.search(r'instance="(.*?)"', column_name)
     return match.group(1)
 
-start_time = '2021-12-03T00:00:00Z'
-end_time = '2021-12-04T16:10:00Z'
+last_day_of_last_month = datetime.today().replace(day=1) - relativedelta(days=1)
+last_month = datetime.today() - relativedelta(months=1)
+beginning_of_last_month = datetime(last_month.year,last_month.month,1,0,0)
+end_of_last_month = datetime(last_month.year,last_month.month,last_day_of_last_month.day,23,59,59)
+
+#start_time = beginning_of_last_month.strftime('%Y-%m-%dT%H:%M:%SZ')
+#end_time = end_of_last_month.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+end_time = datetime.now()
+start_time = end_time - relativedelta(hours=1)
+
 step_time = '5m'
 queries = ['memory_usage_percent','cpu_usage_percent','dns_query_count']
 
 for query in queries:
     df = p.query_range(query,start_time, end_time, step_time)
     df.columns = df.columns.to_series().apply(rename_column)
+    df['average'] = df.mean(axis=1)
     plt.style.use('dark_background')
-    plot = df.plot(title=query)
+    plot = df.plot(title=query, alpha=0.3)
     plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.9), ncol=3)
     plot.get_figure().savefig(query+".png",bbox_inches='tight')
+    plot.lines[-1].set_alpha(1)
+
+myTeamsMessage = pymsteams.connectorcard("<Microsoft Webhook URL>")
+myTeamsMessage.text("this is my text")
+myTeamsMessage.color("blue")
+myTeamsMessage.printme()
